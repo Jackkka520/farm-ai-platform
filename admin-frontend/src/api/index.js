f8a559b7a58b53d5e782e4e7fc5aa3dea903385a
Zhang Cheng <2540310276@qq.com>
2026-04-20 18:15:14 +0800

    update api address

--- admin-frontend/src/api/index.js
+++ admin-frontend/src/api/index.js
@@ -1,8 +1,18 @@
 import axios from 'axios';
 
+//localhost
+// const api = axios.create({
+//     baseURL: 'http://localhost:3001/api/admin',
+//     timeout: 10000,
+// });
+
+
+//test deploy
+const API_BASE_URL = 'https://prissy-agent-pants.ngrok-free.dev/api/admin';
+
 const api = axios.create({
-    baseURL: 'http://localhost:3001/api/admin',
-    timeout: 10000,
+    baseURL: API_BASE_URL,
+    timeout: 30000,
 });
 
 // 仪表盘
